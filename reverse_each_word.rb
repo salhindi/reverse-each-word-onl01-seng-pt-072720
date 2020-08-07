35b15955001aadc953a8c3787da3c75adfd4cb3b
Sara A <calisaraalhindi@gmail.com>
2020-08-07 19:42:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   #split method is string.split().
-  sentence.each do |a|
+  reversed_sentence = []
+  
+  reversed_sentence.each do |a|
     a.reverse 
   end
 end
