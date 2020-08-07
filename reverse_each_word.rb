01124517e4492c5f5d9ceca7c275c9e2cb653cef
Sara A <calisaraalhindi@gmail.com>
2020-08-07 19:43:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   #split method is string.split().
   reversed_sentence = []
   
-  reversed_sentence.each do |a|
+  reversed_sentence.push word.reverse each do |a|
     a.reverse 
   end
 end
