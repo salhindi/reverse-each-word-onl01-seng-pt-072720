e457528700a3c20665b8c44dd65c1921845a5ff6
Sara A <calisaraalhindi@gmail.com>
2020-08-07 19:46:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  #split method is string.split().
+  reversed_sentence = []
+  words.collect do |a|
+   reversed_sentence.push a.reverse 
+  end
+  reversed_sentence.join(" ")
+end
